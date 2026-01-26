eaa19e57c63052fbb9cbf1ed1ad7112d5e730992
raficoco <rafitas2002@gmail.com>
2026-01-26 13:50:38 -0600

    actu

--- crm-airhive/src/lib/supabase-server.ts
+++ crm-airhive/src/lib/supabase-server.ts
@@ -3,7 +3,7 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { Database } from './supabase'
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export const createClient = (cookieStore: any) => {
     return createServerClient<Database>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
